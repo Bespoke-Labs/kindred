d5f66ece1b5a91e9bd0f50ea63d31bac3c7e2203
“seeinplays” <seeinplays@gmail.com>
2022-12-06 21:31:32 -0700

    remove boiler components

--- kindred/packages/frontend/pages/index.tsx
+++ kindred/packages/frontend/pages/index.tsx
@@ -27,8 +27,6 @@ export default function Home() {
           alignItems: 'center',
         }}
       >
-        <GetGreeter />
-        <SetGreeter />
         <Landing />
       </main>
     </div>
