5684ed6fe5765116b246e8d0f22399a90b1b51e1
“seeinplays” <seeinplays@gmail.com>
2022-12-06 21:31:51 -0700

    cleanup

--- kindred/packages/frontend/pages/index.tsx
+++ kindred/packages/frontend/pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head';
 
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { GetGreeter, SetGreeter } from '../components/contract';
 import Landing from './Landing/landing';
 import Navbar from './Navbar/Navbar';
 
